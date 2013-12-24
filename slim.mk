93c0ec51d668d3214638c9839ef1ca7df710d7c2
nicklovell23 <nicklovell23@gmail.com>
2013-12-24 23:12:41 +0100

    Remove common led flash
    Change-Id: Ifb473e19e7402d293bcd648728b327fc1e8fb71b

--- slim.mk
+++ slim.mk
@@ -9,9 +9,6 @@ $(call inherit-product, vendor/slim/config/cdma.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys" PRIVATE_BUILD_DESC="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys"
 
 PRODUCT_NAME := slim_d2usc
