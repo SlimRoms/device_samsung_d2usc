a5d6292b561a6263004e961c29295b10e119af83
nicklovell23 <nicklovell23@gmail.com>
2015-02-20 05:09:31 +0100

    bring d2usc up to date
    Change-Id: I4f1ef13f55101dc5e79b9b8a1c41e522f2e0e218

--- slim.mk
+++ slim.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2usc \
     TARGET_DEVICE=d2usc \
-    BUILD_FINGERPRINT="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys"
+    BUILD_FINGERPRINT="samsung/d2usc/d2usc:5.0.2/LRX22G/R530UVXUBMJA:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2usc-user 5.0.2 LRX22G R530UVXUBMJA release-keys"
 
 PRODUCT_NAME := slim_d2usc
 PRODUCT_DEVICE := d2usc
